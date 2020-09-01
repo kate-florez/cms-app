4b755ee2e50d9dda9ac98dc4891ba8668a11b3d5
Arthur Lurye <arthur.lurye@EUCs-MacBook-Pro-10.local>
2020-09-01 14:04:08 -0400

    working on editor

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EditorComponent } from './editor/editor.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'edit', component: EditorComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [EditorComponent];
---
+++ src/app/editor/editor.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'editor',
+  templateUrl: './editor.component.html',
+  styleUrls: ['./editor.component.css']
+})
+
+export class EditorComponent {
+  title = 'cms-app';
+
+  
+}
